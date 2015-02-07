1910a175f53f73c74eb36cbd575c1c0e51ebccb9
Brij Shah <Brij@Brijs-MacBook-Pro.local>
2015-02-07 00:14:41 -0800

    added checkConnection function to display connections every 15 secs

--- server.rb
+++ server.rb
@@ -5,15 +5,23 @@ DEFAULT_PORT = 8005
 server = TCPServer.new(DEFAULT_PORT)
 
 
- ip = UDPSocket.open {|s| s.connect("64.233.187.99", 1); s.addr.last}
+ip = UDPSocket.open {|s| s.connect("64.233.187.99", 1); s.addr.last}
 port = server.addr[1].to_s
-
-
-puts "Ready to receive on "+ ip +":" + port
-
 @numOfClients = 0
 
 
+def checkConnected
+  var = Thread.new{
+    while(true)
+      sleep 15
+      puts "Clients currently connected: #{@numOfClients}"
+    end
+  }
+end
+
+puts "Ready to receive on "+ ip +":" + port
+
+checkConnected
 
 while (connection = server.accept)
   Thread.new(connection) do |conn|
@@ -21,7 +29,7 @@ while (connection = server.accept)
     client = "#{host}:#{port}"
     puts "#{client} is connected"
     @numOfClients = @numOfClients + 1
-    puts "Clients currently connected: #{@numOfClients}"
+    #puts "Clients currently connected: #{@numOfClients}"
     begin
       loop do
         message = conn.readline
@@ -33,7 +41,7 @@ while (connection = server.accept)
       @numOfClients = @numOfClients - 1
     
       puts "#{client} has disconnected"
-        puts "Clients currently connected: #{@numOfClients}"
+      #puts "Clients currently connected: #{@numOfClients}"
     end
   end
 end
